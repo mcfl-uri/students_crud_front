fe08a34a687412bc4a6901a7e16eaca6005644c4
mcfl-uri <lamelasoriol@gmail.com>
2022-05-31 14:22:46 +0200

    tot

--- src/components/AddStudentForm.js
+++ src/components/AddStudentForm.js
@@ -2,28 +2,38 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import {Button, Container, Paper} from "@mui/material";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function AddStudentForm() {
     const paperStyle = {padding: '50px 20px', width: 600, margin: "20px auto"}
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
+    const [student, setStudent] = useState([])
 
-    const submitForm=(e)=>{
+    const submitForm = (e) => {
         e.preventDefault()
-        const student={name, email}
+        const student = {name, email}
         console.log(student)
         fetch(
             "http://localhost:8080/student/add", {
                 method: "POST",
-                headers:{"Content-Type":"application/json"},
-                body:JSON.stringify(student)
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(student)
             }
         ).then(() => {
             console.log("Student added")
+            window.location.reload(false);
         })
     }
 
+
+    useEffect(() => {
+        fetch("http://localhost:8080/student/all")
+            .then(res => res.json())
+            .then((res) => {
+                setStudent(res);
+            })
+    }, [])
     return (
         <Container>
             <Paper elevation={3} style={paperStyle}>
@@ -42,6 +52,16 @@ export default function AddStudentForm() {
                                onChange={(e) => setEmail(e.target.value)}/>
                     <Button variant="contained" onClick={submitForm}>Submit</Button>
                 </Box>
+            </Paper>
+            <h1>Students</h1>
+            <Paper elevation={3} style={paperStyle}>
+                {student.map(student => (
+                    <Paper elevation={6} style={{margin: "10px", padding: "15px", textAlign: "left"}} key={student.id}>
+                        Id: {student.id}<br/>
+                        Name: {student.name}<br/>
+                        Email: {student.email}
+                    </Paper>
+                ))}
             </Paper>
         </Container>
     );
